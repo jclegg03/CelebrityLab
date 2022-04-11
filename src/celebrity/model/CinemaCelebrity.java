fc22dbb4912a9c80c4fd01e9fa47b4913d9e9c90
jclegg03 <jjc2003@comcast.net>
2022-04-11 12:12:39 -0600

    Added getClue, getMovie, and toString

--- src/celebrity/model/CinemaCelebrity.java
+++ src/celebrity/model/CinemaCelebrity.java
@@ -24,4 +24,40 @@ public class CinemaCelebrity extends Celebrity
 			movies.add(clue.split(":")[1]);
 		}
 	}
+	
+	@Override
+	public String getClue()
+	{
+		if(clues.size() == 0)
+		{
+			seperateClues();
+		}
+		String clue = clues.remove((int) (Math.random() * clues.size()));
+		
+		return clue;
+	}
+	
+	public String getMovie()
+	{
+		if(movies.size() == 0)
+		{
+			seperateClues();
+		}
+		String movie = movies.remove((int) (Math.random() * clues.size()));
+		
+		return movie;
+	}
+	
+	@Override
+	public String toString()
+	{
+		String description = "This is a movie celebrity named: " + getAnswer() + "\nHere are some characters and movies they are associated with:";
+		seperateClues();
+		for(String clue : clues)
+		{
+			description += "\n" + clue;
+		}
+		
+		return description;
+	}
 }
